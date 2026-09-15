8a9ee4e32fe8d82c22e4c2f44109676f167c8bfb
aliakseidrob <aliakseidrob@gmail.com>
2026-09-15 16:18:32 +0300

    feat: solution getCardId

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -483,8 +483,27 @@ function encodeToRot13(/* str */) {
  *   'Q♠' => 50
  *   'K♠' => 51
  */
-function getCardId(/* value */) {
-  throw new Error('Not implemented');
+function getCardId(value) {
+  const RANKS = [
+    'A',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    '10',
+    'J',
+    'Q',
+    'K',
+  ];
+  const SUITS = ['♣', '♦', '♥', '♠'];
+
+  const rank = value.slice(0, -1);
+  const suit = value.slice(-1);
+  return SUITS.indexOf(suit) * 13 + RANKS.indexOf(rank);
 }
 
 module.exports = {
