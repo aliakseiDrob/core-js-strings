0bd53202c04e6c9c295d3e514a1c96fbcfc6f585
aliakseidrob <aliakseidrob@gmail.com>
2026-09-15 15:35:54 +0300

    feat: formatTime

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -225,8 +225,11 @@ function endsWith(str, substr) {
  *   formatTime(0, 45) => "00:45"
  *   formatTime(0, 0) => "00:00"
  */
-function formatTime(/* minutes, seconds */) {
-  throw new Error('Not implemented');
+function formatTime(minutes, seconds) {
+  const paddedMinutes = String(minutes).padStart(2, '0');
+  const paddedSeconds = String(seconds).padStart(2, '0');
+
+  return `${paddedMinutes}:${paddedSeconds}`;
 }
 
 /**
